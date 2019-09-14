4880407becdb41fb5aec1ab0c09dd0a78a648176
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-14 23:41:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,8 +19,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-  # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-  
   programmer_hash = 
     {
         :grace_hopper => {
@@ -55,17 +53,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-
 	programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
   programmer_hash = 
     {
         :grace_hopper => {
@@ -81,7 +72,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
 		 programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
@@ -104,9 +94,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-
 	programmer_hash[:alan_kay][:known_for] = alans_new_info
 	programmer_hash
 end
